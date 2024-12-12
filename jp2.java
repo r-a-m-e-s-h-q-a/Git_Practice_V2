92cfa5f3f59728f13df0ca7823112089320bfe52
r-a-m-e-s-h-q-a <ramesh.qa8795@gmail.com>
2024-12-12 17:37:56 +0530

    Add files via upload
    First commit

---
+++ jp2.java
@@ -0,0 +1,49 @@
+
+public class GitPractice_File1 {
+	
+	public void method1()
+	{
+		System.out.println("From method 1");
+		
+	}
+	
+	public void method2()
+	{
+		System.out.println("from method 2");
+		
+	}
+	
+	public void method3()
+	{
+		System.out.println("from method 3");
+		
+	}
+	public void method4()
+	{
+		System.out.println("from method 4");
+		
+	}
+
+	public void method5()
+	{
+		System.out.println("from method 5");
+		
+	}
+	
+	public void method6()
+	{
+		System.out.println("from method 6");
+		
+	}
+
+
+	public static void main(String[] args) {
+		
+		GitPractice_File1 gp = new GitPractice_File1();
+		gp.method1();
+	
+		
+	}
+	
+
+}
